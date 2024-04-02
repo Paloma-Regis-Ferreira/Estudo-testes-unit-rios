d38475f103d74cc231691d6f396e8dd8c5722ab3
PalomaRFerreira <tqi_pferreira@uolinc.com>
2024-04-02 10:42:17 -0300

    Jenkinsfile teste

--- src/test/java/com/estudodetestes/GerenciadorFinanceiroJunit5/barriga/domain/UsuarioTest.java
+++ src/test/java/com/estudodetestes/GerenciadorFinanceiroJunit5/barriga/domain/UsuarioTest.java
@@ -86,7 +86,7 @@ class UsuarioTest {
         ValidationException ex = assertThrows(ValidationException.class, () ->
                 umUsuario().comEmail(null).agora()
         );
-        assertEquals("Email é obrigatori", ex.getMessage());
+        assertEquals("Email é obrigatorio", ex.getMessage());
     }
 
     @Test
